74be709fcb348540656a8b18b166475c7258a06e
KOTEY Jonathan Nikoi <32402172+kofilolx@users.noreply.github.com>
2024-11-01 04:19:24 +0000

    DB file

---
+++ wickStore-mern/backend/config/database.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+/* 
+Set the strict query mode, which defines how strictly mongoose should apply filters
+when querying the database. Adjust this setting based on application requirements.
+*/
+mongoose.set('strictQuery', false);
+
+/* 
+Define a function to connect to the MongoDB database. This function uses
+the MongoDB URI stored in environment variables to initiate the connection.
+*/
+const connectDatabase = () => {
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,       // Recommended parser to handle MongoDB connection strings
+        useUnifiedTopology: true,    // Improved connection management
+    })
+    .then(() => {
+        /* Log a success message if the connection to MongoDB is established */
+        console.log('MongoDB connected');
+    })
+    .catch(err => {
+        /* 
+        Log an error message if there’s a problem connecting to MongoDB.
+        Exits the process with a failure status code to ensure visibility of the issue.
+        */
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1);
+    });
+};
+
+/* Export the connectDatabase function for use in other parts of the application */
+module.exports = connectDatabase;
