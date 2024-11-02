83ab6088714e332ce68a2f8b01fd547cdbebb264
KOTEY Jonathan Nikoi <32402172+kofilolx@users.noreply.github.com>
2024-11-02 01:52:50 +0000

    README upDate

--- wickStore-mern/gC.sh
+++ wickStore-mern/gC.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-read -p "Enter the filename to commit: " filename
-
-# Check if the file exists
-if [ ! -f "$filename" ]; then
-    echo "File not found: $filename"
-    exit 1
-fi
-
 read -p "Enter your commit message here: " commit_msg
 
-git add "$filename"
+git add .
 git commit -m "$commit_msg"
 git push
